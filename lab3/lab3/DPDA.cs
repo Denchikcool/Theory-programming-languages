858a96f851c7f09354e21c4a15586289af24803b
Denchikcool <kuzmenok.den@yandex.ru>
2024-11-11 22:32:00 +0700

    Реализован детерминированный автомат с магазинной памятью (стеком)

---
+++ lab3/lab3/DPDA.cs
@@ -0,0 +1,117 @@
+﻿using System.Text.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace lab3
+{
+    public class DPDA
+    {
+        private readonly List<string> _states;
+        private readonly List<string> _inputAlphabet;
+        private readonly List<string> _stackAlphabet;
+        private readonly Dictionary<string, List<string>> _transitions;
+        private readonly string _initialState;
+        private readonly string _initialStackSymbol;
+        private readonly List<string> _acceptingStates;
+
+        private string _currentState;
+        private Stack<string> _stack;
+        public List<string> History { get; }
+
+        public DPDA() { }
+        public DPDA(
+            List<string> states,
+            List<string> inputAlphabet,
+            List<string> stackAlphabet,
+            Dictionary<string, List<string>> transitions,
+            string initialState,
+            string initialStackSymbol,
+            List<string> acceptingStates)
+        {
+            _states = states;
+            _inputAlphabet = inputAlphabet;
+            _stackAlphabet = stackAlphabet;
+            _transitions = transitions;
+            _initialState = initialState;
+            _initialStackSymbol = initialStackSymbol;
+            _acceptingStates = acceptingStates;
+
+            History = new List<string>();
+        }
+
+        public (bool Success, string Reason) Accept(string inputString)
+        {
+            Reset();
+
+            foreach (char symbol in inputString)
+            {
+                var character = Step(symbol.ToString());
+                if (!character.Success)
+                {
+                    return character;
+                }
+            }
+
+            var stepResult = Step("");
+            if (!stepResult.Success)
+            {
+                return stepResult;
+            }
+            if (_acceptingStates.Contains(_currentState))
+            {
+                return (true, "");
+            }
+            return (false, "Цепочка не завершилась в принимающем состоянии");
+        }
+
+        public void Reset()
+        {
+            _currentState = _initialState;
+            _stack = new Stack<string>();
+            _stack.Push(_initialStackSymbol);
+            History.Clear();
+            History.Add($"Начальное состояние: {_currentState}, стек: {_initialStackSymbol}");
+        }
+
+        private (bool Success, string Reason) Step(string symbol)
+        {
+            if (!_inputAlphabet.Contains(symbol) && symbol != "")
+            {
+                return (false, $"В цепочке присутствуют посторонние символы (символа \"{symbol}\" нет в алфавите)!");
+            }
+
+            var stackTop = _stack.Count > 0 ? _stack.Peek() : null;
+            var transitionKey = $"({_currentState}, {symbol}, {stackTop})";
+
+            if (!_transitions.ContainsKey(transitionKey))
+            {
+                return (false, $"Нет перехода для ({_currentState}, {symbol}, {stackTop})");
+            }
+
+            var transition = _transitions[transitionKey];
+            var nextState = transition[0];
+            var newStackSymbols = transition[1];
+
+            _currentState = nextState;
+            if (_stack.Count > 0)
+            {
+                _stack.Pop();
+            }
+
+            foreach (var newStackSymbol in newStackSymbols.Reverse())
+            {
+                _stack.Push(newStackSymbol.ToString());
+            }
+
+            History.Add($"Текущее состояние: {_currentState}, символ: {symbol} стек: {string.Join(", ", _stack.ToArray())}");
+            return (true, "");
+        }
+    }
+}
+
+
---
+++ lab3/lab3/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DPDA _dpda;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadAutomaton(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "JSON files (*.json)|*.json"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    _dpda = LoadAutomatonFromFile(openFileDialog.FileName);
+                    MessageBox.Show("Автомат загружен успешно!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка загрузки автомата: {ex.Message}");
+                }
+            }
+        }
+
+        private DPDA LoadAutomatonFromFile(string filepath)
+        {
+            string json;
+            using (var reader = new StreamReader(filepath))
+            {
+                json = reader.ReadToEnd();
+            }
+
+            var data = JsonConvert.DeserializeObject<AutomatonData>(json);
+
+            var dpda =  new DPDA(
+                data.States,
+                data.InputAlphabet,
+                data.StackAlphabet,
+                data.Transitions,
+                data.InitialState,
+                data.InitialStackSymbol,
+                data.AcceptingStates
+            );
+            dpda.Reset();
+            return dpda;
+        }
+
+        private void CheckString(object sender, RoutedEventArgs e)
+        {
+            if (_dpda == null)
+            {
+                MessageBox.Show("Сначала загрузите автомат!");
+                return;
+            }
+
+            HistoryTextBlock.Text = "";
+
+            var inputString = InputTextBox.Text;
+
+            var result = _dpda.Accept(inputString);
+
+            MessageBox.Show(result.Success ? "Цепочка принята" : $"Цепочка отклонена: {result.Reason}");
+
+            HistoryTextBlock.Text = string.Join("\n", _dpda.History);
+        }
+
+        public class AutomatonData
+        {
+            [JsonProperty("states")]
+            public List<string> States { get; set; }
+            [JsonProperty("input_alphabet")]
+            public List<string> InputAlphabet { get; set; }
+            [JsonProperty("stack_alphabet")]
+            public List<string> StackAlphabet { get; set; }
+            [JsonProperty("transitions")]
+            public Dictionary<string, List<string>> Transitions { get; set; }
+            [JsonProperty("initial_state")]
+            public string InitialState { get; set; }
+            [JsonProperty("initial_stack_symbol")]
+            public string InitialStackSymbol { get; set; }
+            [JsonProperty("accepting_states")]
+            public List<string> AcceptingStates { get; set; }
+        }
+    }
+}
